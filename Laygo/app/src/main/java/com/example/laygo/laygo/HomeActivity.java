8dac07e37f8dda533c5247d1a24d8157218e9989
Superka <paul.choisel@insa-lyon.fr>
2016-11-13 11:06:28 +0000

    Revert "Merge branch 'master' of https://github.com/layderv/laygo"
    This reverts commit acb6c8bf82a9ba7e94d82ebac6dd14ed2071cf04, reversing
    changes made to 5af029c6b0c5747bbac49d526d9027a80f5d0a29.

--- Laygo/app/src/main/java/com/example/laygo/laygo/HomeActivity.java
+++ Laygo/app/src/main/java/com/example/laygo/laygo/HomeActivity.java
@@ -4,10 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
-import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -17,34 +13,5 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
-            Intent intent = new Intent(this, SearchableActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void startAddBrick (View view) {
-        Intent intent = new Intent(this, AddBrick.class);
-        startActivity(intent);
     }
 }
