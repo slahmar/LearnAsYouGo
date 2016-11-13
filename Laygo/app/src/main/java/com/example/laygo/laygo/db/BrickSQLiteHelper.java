515eacee06c60e5ffd8ddb7ef213bc2bc58fa466
Superka <paul.choisel@insa-lyon.fr>
2016-11-13 11:12:59 +0000

    Revert "Add BlockDAO and refactor a bit. Created the add block activity, empty for now"
    This reverts commit 5af029c6b0c5747bbac49d526d9027a80f5d0a29.

--- Laygo/app/src/main/java/com/example/laygo/laygo/db/BrickSQLiteHelper.java
+++ Laygo/app/src/main/java/com/example/laygo/laygo/db/BrickSQLiteHelper.java
@@ -5,41 +5,35 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class LaygoSQLiteHelper extends SQLiteOpenHelper {
+/**
+ * Created by salom_000 on 19/10/2016.
+ */
+
+public class BrickSQLiteHelper extends SQLiteOpenHelper {
     public static final String TABLE_BRICK = "brick";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_WORD = "word";
-    public static final String COLUMN_TRANSLATION = "translation";
-
-    public static final String TABLE_BLOCK = "block";
-    public static final String COLUMN_NAME = "name";
 
     private static final String DATABASE_NAME = "brick.db";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String CREATE_TABLE_BRICK = "create table "
+    private static final String DATABASE_CREATE = "create table "
             + TABLE_BRICK + "(" + COLUMN_ID
             + " integer primary key autoincrement, " + COLUMN_WORD
-            + " text not null, "+ COLUMN_TRANSLATION +" );";
-
-    private static final String CREATE_TABLE_BLOCK = "create table "
-            + TABLE_BLOCK + "(" + COLUMN_ID
-            + " integer primary key autoincrement, " + COLUMN_NAME
             + " text not null);";
 
-    public LaygoSQLiteHelper(Context context) {
+    public BrickSQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(CREATE_TABLE_BRICK);
-        database.execSQL(CREATE_TABLE_BLOCK);
+        database.execSQL(DATABASE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.w(LaygoSQLiteHelper.class.getName(),
+        Log.w(BrickSQLiteHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_BRICK);
