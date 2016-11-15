81e2c279aae9570f89550599ac63c0b8edf8b4bf
l <@>
2016-11-15 22:39:37 +0000

    QuizActivity: almost done, needs to retrieve activities from the database (and update them when the quiz is over); need to go to the ResultActivity when the quiz is over

--- Laygo/app/src/main/java/com/example/laygo/laygo/model/Question.java
+++ Laygo/app/src/main/java/com/example/laygo/laygo/model/Question.java
@@ -3,17 +3,50 @@ package com.example.laygo.laygo.model;
 /**
  * This class represents a question about a certain brick
  */
-public abstract class Question {
-    /**
-     * The word about which the question is
-     */
+public class Question implements Comparable {
+
     private Brick brick;
+
+    private int asked = 0;
+
+    private int correct = 0;
+
+    public Question(Brick b) {brick = b;}
+    public Question() {this(null);}
     /**
      * Number of times the question has been asked
      */
-    private int asked = 0;
+    public int getAsked() {return asked;}
     /**
      * Number of times the question was correctly answered
      */
-    private int correct = 0;
+    public int getCorrect() {return correct;}
+    /**
+     * The word about which the question is
+     */
+    public Brick getBrick() {return brick;}
+
+    public void setAsked(int v) {asked = v;}
+    public void setCorrect(int v) {correct = v;}
+    public void setBrick(Brick b) {brick = b;}
+
+    public void incAsked() {asked++;}
+    public void incCorrect() {correct++;}
+
+    /**
+     * Precedence: this has precedence if it has been asked not more than q2;
+     *             if the same, it has precedence if it has been answered correctly fewer times
+     * @param q the other question to be compared this to
+     * @return
+     */
+    public int compareTo(Object q) {
+        Question q2 = (Question)q;
+        int r = getAsked() - q2.getAsked();
+        return r != 0 ? r : getCorrect() - q2.getCorrect();
+    }
+
+    public String toString() {
+        return brick.getWord();
+    }
+
 }
