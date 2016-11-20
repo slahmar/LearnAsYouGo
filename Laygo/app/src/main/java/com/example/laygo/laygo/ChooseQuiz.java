8c35dbd094ac19463c96a91612be94d547b5423c
layderv <d.ahmed@studenti.unipi.it>
2016-11-20 11:13:25 +0000

    Update ChooseQuiz.java

--- Laygo/app/src/main/java/com/example/laygo/laygo/ChooseQuiz.java
+++ Laygo/app/src/main/java/com/example/laygo/laygo/ChooseQuiz.java
@@ -9,6 +9,15 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.laygo.laygo.dao.BrickDAO;
+import com.example.laygo.laygo.model.Brick;
+import com.example.laygo.laygo.model.Question;
+import com.example.laygo.laygo.model.Quiz;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static com.example.laygo.laygo.R.styleable.View;
 
@@ -21,13 +30,39 @@ public class ChooseQuiz extends AppCompatActivity {
     }
 
     public void onClickTextQuiz(View view) {
-        Intent intent = new Intent(this, TextQuizActivity.class);
-        startActivity(intent);
+        List<Question> tmp = new LinkedList<>();
+        BrickDAO bdao = new BrickDAO(getApplicationContext());
+        bdao.open();
+        List<Brick> bricks = bdao.findAll();
+        for (Brick b : bricks) {
+            tmp.add(new Question(b));
+        }
+        if (tmp.size() < Quiz.MIN_TEXTS) {
+            Toast.makeText(this, "You don't have enough words!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Intent intent = new Intent(this, TextQuizActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void onClickPictureQuiz(View view) {
-        Intent intent = new Intent(this, GalleryQuiz.class);
-        startActivity(intent);
+        List<Question> tmp = new LinkedList<>();
+        BrickDAO bdao = new BrickDAO(getApplicationContext());
+        bdao.open();
+        List<Brick> bricks = bdao.findAll();
+        for (Brick b : bricks) {
+            if (b.getImage() == null || b.getImage().equals("")) continue;
+            tmp.add(new Question(b));
+        }
+        
+        if (tmp.size() < Quiz.MIN_PICTURES) {
+            Toast.makeText(this, "You don't have enough words!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Intent intent = new Intent(this, GalleryQuiz.class);
+            startActivity(intent);
+        }
     }
 
 
