84472347ffebc0dd25ef33550f799593b9a07ec5
Salomé LAHMAR <Salom LAHMAR>
2016-11-30 12:52:03 +0000

    Try to delete bricks, improve design and replace icons

--- Laygo/app/src/main/java/com/example/laygo/laygo/activity/MapsActivity.java
+++ Laygo/app/src/main/java/com/example/laygo/laygo/activity/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.laygo.laygo.activity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.laygo.laygo.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
