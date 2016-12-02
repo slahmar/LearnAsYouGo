98a2208744d1c6ed4adc8656c775135530363706
l <@>
2016-12-02 12:01:48 +0000

    TextQuiz working, passing results working; GalleryQuiz to be tested

--- Laygo/app/src/main/java/com/example/laygo/laygo/activity/QuizResultActivity.java
+++ Laygo/app/src/main/java/com/example/laygo/laygo/activity/QuizResultActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.laygo.laygo.HomeActivity;
@@ -12,6 +14,8 @@ import com.example.laygo.laygo.R;
 
 public class QuizResultActivity extends AppCompatActivity {
     private String PREFS = "Settings";
+    private String givenAnswers, correctAnswers, askedQuestionsIDs;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +24,28 @@ public class QuizResultActivity extends AppCompatActivity {
 
         int score = 0;
         int nbQuestions = 1;
+        String[] given, corrects, ids;
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             score = extras.getInt("SCORE");
             nbQuestions = extras.getInt("NBQUESTIONS");
+            givenAnswers = extras.getString("GIVEN_ANSWERS");
+            correctAnswers = extras.getString("CORRECT_ANSWERS");
+            askedQuestionsIDs = extras.getString("ASKED_IDS");
         }
         if (score < 0) throw new IllegalArgumentException("Score negative");
 
         String result = score + "/" + nbQuestions;
         TextView tv = (TextView) findViewById(R.id.scorePoints);
         tv.setText(result);
+
+        given = givenAnswers.split("|");
+        corrects = correctAnswers.split("|");
+        ids = askedQuestionsIDs.split("|");
+        for (int i = 0; i < given.length; ++i)
+            Log.e("ANSW", "Given:" + given[i] + ", corr:" + corrects[i]);
+
     }
 
     @Override
